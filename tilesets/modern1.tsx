579a07fb2a892222dd7e868b78fa0275358a0df7
Simon Dohmen <noerk.sdo@gmail.com>
2021-02-18 13:42:31 +0100

    .

--- tilesets/modern1.tsx
+++ tilesets/modern1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="modern1" tilewidth="32" tileheight="32" tilecount="960" columns="16">
- <image source="Downloads/Modern_Interiors/1_Interiors/32x32/Theme_Sorter_32x32/1_Generic_32x32.png" width="512" height="1920"/>
+ <image source="1_Generic_32x32.png" width="512" height="1920"/>
 </tileset>
