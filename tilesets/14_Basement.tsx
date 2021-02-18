cf14ba87217426ae847c471cfbfc1a6e49037d46
Simon Dohmen <noerk.sdo@gmail.com>
2021-02-18 13:22:05 +0100

    .

---
+++ tilesets/14_Basement.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="14_Basement" tilewidth="32" tileheight="32" tilecount="424" columns="8">
+ <image source="14_Basement.png" width="256" height="1696"/>
+</tileset>
