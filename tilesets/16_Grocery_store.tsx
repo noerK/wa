ec58eefc5336675e1eff248b63c97fdecc6679c6
noerK <noerk.sdo@googlemail.com>
2021-02-19 12:49:25 +0000

    Deploying to gh-pages from master @ 40504c80993528d10c7f3e7926f2d5b3ead98f24 🚀

--- tilesets/16_Grocery_store.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="16_Grocery_store" tilewidth="32" tileheight="32" tilecount="888" columns="8">
- <image source="16_Grocery_store.png" width="256" height="3552"/>
-</tileset>
--- tilesets/Tilesets_32x32.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Tilesets_32x32" tilewidth="32" tileheight="32" tilecount="2400" columns="40">
- <image source="Tilesets_32x32.png" width="1280" height="1920"/>
- <tile id="1">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
